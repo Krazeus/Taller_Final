3ecdae3aee31bf3530d1d096256ee9fc24e29042
ANTHONY ISAAC GARCIA CHACHA <anthony.garciac@ug.edu.ec>
2021-06-01 15:54:35 -0500

    Agregar archivos de proyecto.

---
+++ Taller_Final/Taller_Final.Android/Splash.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Taller_Final.Droid
+{
+    [Activity(Label = "AMPH" ,Theme = "@style/slash_theme", MainLauncher =true,NoHistory =true,ConfigurationChanges = Android.Content.PM.ConfigChanges.ScreenSize)]
+    public class Splash : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+
+            // Create your application here
+        }
+    }
+}
---
+++ Taller_Final/Taller_Final/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Taller_Final
+{
+  
+
+    [Obsolete, DesignTimeVisible(false)]
+    public partial class MainPage : MasterDetailPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Mymenu();
+        }
+        public void Mymenu()
+        {
+            Detail = new NavigationPage(new Home());
+            List<Menu> Menu = new List<Menu>
+            {
+            new Menu{ Page = new Home(), MenuTitle ="Home"},
+            new Menu{ Page = new Cuenta(), MenuTitle ="Cuenta"},
+            new Menu{ Page = new Contactanos(), MenuTitle ="Contactanos"},
+            new Menu{ Page = new Ubicanos(), MenuTitle ="Ubicanos"},
+            };
+            ListMenu.ItemsSource = Menu;
+        }
+        public void ListaMenu_SelectedItem(object sender, SelectedItemChangedEventArgs e)
+        {
+            var menu = e.SelectedItem as Menu;
+            if(menu != null)
+            {
+                IsPresented = false;
+                Detail = new NavigationPage(menu.Page);
+            }
+          
+        }
+
+        public class Menu
+        {
+            public string MenuTitle { get; set; }
+            public Page Page { get; set; }
+        }
+    }
+}
